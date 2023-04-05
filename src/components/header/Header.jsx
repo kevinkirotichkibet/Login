ce7b90122af3b3bf62b2f7e05adf1f788ffa7279
kevinkirotichkibet <kirotichkevin@gmail.com>
2023-04-05 21:24:51 +0300

    Logo and styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import './header.css'
 
-import MyLogo from './../../3.png'
-
+// import MyLogo from './../../3.png'
+import Logo from './../../assets/road_logo.svg'
 
 export default function Header() {
   return (
     <div className='header'>
         <div className="headerWrapper">
             <div className="headerLogoContainer">
-            <img src={MyLogo} alt="" className="logoImage" />
+            <img src={Logo} alt="" className="logoImage" />
             <h2 className="headerTitleContainer">Njiani</h2>
             </div>
             {/* <div className="headerTitle">
