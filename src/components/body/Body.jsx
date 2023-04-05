1229de37505911e634421761a0cd36cf0b03d50c
Kevin Kirotich Kibet <kirotichkevin@gmail.com>
2023-04-05 16:25:43 +0300

    username and password icon added

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import './body.css'
 import Button from '@mui/material/Button';
+// import image from './../../my_logo.svg'
 import PersonIcon from '@mui/icons-material/Person';
+import LockIcon from '@mui/icons-material/Lock';
+// import Icon from '@mui/material/Icon';
+// import {Notifications} from "@mui/icons-material"
+
 export default function Body() {
   return (
     <div className='body'>
@@ -14,15 +19,16 @@ export default function Body() {
                 <span className="bodyUsernameTitle">Username</span>
                 </div>
                 <div className="bodyUsernameContainer">
-                    {/* <img src={PersonIcon} alt="" className="bodyUsernameIcon" /> */}
+                    <PersonIcon />
                     <input placeholder='Username' className="bodyUsernameInput" />
-                </div><div className="bodyPasswordContainer">
+                </div>
 
                 <div className="bodyTitleContainer">
                 <span className="bodyUsernameTitle">Password</span>
                 </div>
 
-                    <img src="" alt="" className="bodyPasswordIcon" />
+                <div className="bodyPasswordContainer">
+                    <LockIcon className='passwordIcon'/>
                     <input placeholder='Password' type='password' className="bodyPasswordInput" />
                 </div>
                 <div className="bodyLoginButtonContainer">
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,11 +10,11 @@ export default function Header() {
         <div className="headerWrapper">
             <div className="headerLogoContainer">
             <img src={MyLogo} alt="" className="logoImage" />
-            
-            </div>
-            <div className="headerTitle">
             <h2 className="headerTitleContainer">Njiani</h2>
             </div>
+            {/* <div className="headerTitle">
+            
+            </div> */}
         </div>
       
     </div>
