83225569ca01bb52abe984924ee7412a2710ed72
Kevin Kirotich Kibet <kirotichkevin@gmail.com>
2023-04-14 16:24:26 +0300

    Day Commit

--- src/components/body/BodySignUp.jsx
+++ src/components/body/BodySignUp.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import './bodySignUp.css'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import Button from '@mui/material/Button';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 export default function BodySignUp() {
   return (
     <> 
       <div className="bodySignUp">
         <div className="bodyWrapperLeft">
-          
+          <div className="bodyLeftContainer">
+            <h5 className="containerLeftTitle">Or sign in with...</h5>
+            <div className="facebookContainer">
+            <Button className='facebookLoginButton' variant='contained'><FacebookIcon className='facebookIcon' />Facebook</Button>
+            </div>
+            <div className="twitterContainer">
+            <Button className='twitterLoginButton' variant='contained' style={{ backgroundColor: '#1C99E6' }} ><TwitterIcon className='twitterIcon' />Twitter</Button>
+            </div>
+          </div>
+          <div className="verticalLine"><hr className='bodyContainerHr'></hr></div>
+
         </div>
         <div className="bodyWrapperRight">
-
+          Right
         </div>
 
 
--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './signUp.css'
 import HeaderSignUp from '../../components/header/HeaderSignUp'
+import BodySignUp from '../../components/body/BodySignUp'
 
 
 export default function SignUp() {
   return (
     <div>
       <HeaderSignUp/>
+      <BodySignUp />
     </div>
   )
 }
